00d660ad382014feb50bcc15ccb73f7a9ae796e8
Olivier Albiez <olivier.albiez@poloper.org>
2014-11-21 21:22:07 +0100

    playing with etcd test

--- test/test_etcd.rb
+++ test/test_etcd.rb
@@ -23,9 +23,28 @@ class Container
     def address
         @container.json['NetworkSettings']['IPAddress']
     end
+end
 
-    def http(options={})
-        Net::HTTP.get(URI("http://#{address}:#{options[:port] || 80}/#{options[:path] || ''}"))
+class HttpConnection
+    def initialize(container, options={})
+        @container = container
+        @port = options[:port] || 80
+        @http = Net::HTTP.new @container.address, @port
+    end
+
+    def request(name, options={})
+        data = options[:data]
+        r = Net::HTTPGenericRequest.new(name, (data ? true : false), true, options[:path], options[:header])
+        #r.body = data
+        res = @http.request r, "polop"
+        puts res.body.inspect
+        #puts "#{name} #{options[:data]}"
+        #@http.send_request(name, options[:path], options[:data], options[:header])
+        res
+    end
+
+    def get(path)
+        request('GET', path: path).body
     end
 end
 
@@ -34,14 +53,14 @@ module ContainerPlugin
         super
         containers if respond_to? :containers
         instance_variables.map {|name| instance_variable_get name }.each do |value|
-            value.start if value.instance_of? Container
+            value.start if value.respond_to? :start
         end
     end
 
     def after_teardown
         super
         instance_variables.map {|name| instance_variable_get name }.each do |value|
-            value.stop if value.instance_of? Container
+            value.stop if value.respond_to? :stop
         end
     end
 end
@@ -55,7 +74,14 @@ describe "Etcd" do
     end
 
     it "must have the correct version" do
-        @etcd.http(port: 4001, path: 'version').must_equal 'etcd 0.5.0-alpha.1'
+        sleep 1
+        HttpConnection.new(@etcd, port: 4001).get('/version').must_equal 'etcd 0.5.0-alpha.1'
     end
 
+    it "must store and restore value" do
+        sleep 1
+        connection = HttpConnection.new(@etcd, port: 4001)
+        puts connection.request('PUT', path: '/v2/keys/mykey', data: 'this is awesome').inspect
+        connection.get('/v2/keys/mykey').must_equal 'this is awesome'
+    end
 end
