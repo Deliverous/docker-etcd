f1b227df9a58e572674121e0dbdff0fb0f3b97b3
Olivier Albiez <olivier.albiez@poloper.org>
2014-11-22 14:56:18 +0100

    etcd boottime set to 1 seconde

--- test/test_etcd.rb
+++ test/test_etcd.rb
@@ -7,16 +7,14 @@ describe "Etcd" do
     include DockerTdd::ContainerPlugin
 
     def containers
-        @etcd = DockerTdd::Container.new "registry.deliverous.net/deliverous/etcd:0.5.0-alpha.1-1", args: ["-bind-addr", "0.0.0.0:4001"]
+        @etcd = DockerTdd::Container.new "registry.deliverous.net/deliverous/etcd:0.5.0-alpha.1-1", args: ["-bind-addr", "0.0.0.0:4001"], boottime: 1
     end
 
     it "must have the correct version" do
-        sleep 1
         RestClient.get(url('/version')).body.must_equal 'etcd 0.5.0-alpha.1'
     end
 
     it "must store and restore value" do
-        sleep 1
         RestClient.put(url('/v2/keys/message'), 'value=the value')
         RestClient.get(url('/v2/keys/message')).body.must_equal "{\"action\":\"get\",\"node\":{\"key\":\"/message\",\"value\":\"the value\",\"modifiedIndex\":4,\"createdIndex\":4}}\n"
     end
